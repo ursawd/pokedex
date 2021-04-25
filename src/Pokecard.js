fe92de27828a7c8d9c9d936d7bab49ff3089e762
Phil Bailey <philbailey@msn.com>
2021-04-25 18:30:23 -0400

    before mod Pokedex for layout

---
+++ src/Pokecard.js
@@ -0,0 +1,22 @@
+import "./Pokecard.css";
+function Pokecard(props) {
+  console.log("Entering Pokecard");
+  console.log("Pokecard props", props);
+  const { id, name, type, base_experience } = props.pokemon;
+  console.log(id, name, type, base_experience);
+  return (
+    <div class="poke-card">
+      <h4>{name}</h4>
+      <img
+        class="poke-card-img"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        alt="pokemon"
+      />
+
+      <p>Type: {type}</p>
+      <p>EXP: {base_experience}</p>
+    </div>
+  );
+}
+
+export default Pokecard;
